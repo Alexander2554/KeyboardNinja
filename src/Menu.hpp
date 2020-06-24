5f8706c16d2142a2d080606df5a39a74489ad227
ozzy1337 <officialbait@yandex.ru>
2020-06-24 14:57:59 +0700

    Add hpp for Menu.cpp

---
+++ src/Menu.hpp
@@ -0,0 +1,8 @@
+#ifndef H_MENU
+#define H_MENU
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+sf::RenderWindow window(sf::VideoMode(1000, 750), "KeybordNinja");
+#endif
